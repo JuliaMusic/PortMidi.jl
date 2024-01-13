925f355f520536836b26ce96b0c22a05fe7861f8
Documenter.jl <documenter@juliadocs.github.io>
2024-01-13 20:02:47 +0000

    build based on a7f7bd7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PortMidi","category":"page"},{"location":"#PortMidi","page":"Home","title":"PortMidi","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PortMidi.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PortMidi]","category":"page"}]
+}
